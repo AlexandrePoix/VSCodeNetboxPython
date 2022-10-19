0ddfd618f14484ec635964534387e4f362a4950f
AlexandrePoix <115186238+AlexandrePoix@users.noreply.github.com>
2022-10-19 10:45:00 +0200

    Update GET_tenant.py

--- GET_tenant.py
+++ GET_tenant.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-url = "https://demo.netbox.dev/api/tenancy/tenants/"
+url = "<https://yoururl/api/tenancy/tenants/>"
 
 payload={}
 headers = {
   'accept': 'application/json',
   'Content-Type': 'application/json',
-  'Authorization': 'Token a75dea885edbf297a849e321d1f748bca889027d',
+  'Authorization': 'Token <Your_Token_API>',
   'Cookie': 'csrftoken=PG9P7OdJEEVYjGvX9p84KF7iSrzlqEN4BlFjC8Stz5y5XHOBUW65WtO1uY3BNI1y'
 }
 
