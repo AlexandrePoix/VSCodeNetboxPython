f01f96a11a6776a201d6ef5fb3351464069bbf13
AlexandrePoix <115186238+AlexandrePoix@users.noreply.github.com>
2022-10-19 14:01:58 +0200

    Create POST_VLANs.py

---
+++ POST_VLANs.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+url = "<https://yoururl/api/ipam/vlans/>"
+
+payload = json.dumps({
+  "vid": "100",
+  "name": "vlantestagro1",
+  "tenant": "14"
+})
+headers = {
+  'accept': 'application/json',
+  'Authorization': 'Token <YOUR_TOKEN_API>',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
